3d643fb29a81b587fcdea8ef343d191d1a2e6eb7
Michał Sala <0havaker@gmail.com>
2020-06-29 23:33:09 +0200

    odpalanie za pomocą docker-compose

--- matchmaker/matchmaker.py
+++ matchmaker/matchmaker.py
@@ -1,5 +1,11 @@
-import json
+from __future__ import print_function
 from kafka import KafkaConsumer, KafkaProducer
+import sys
+import json
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+    sys.stderr.flush()
 
 def pairwise(iterable):
     "s -> (s0, s1), (s2, s3), (s4, s5), ..."
@@ -11,18 +17,22 @@ json_serializer = lambda m : json.dumps(m).encode('utf-8')
 
 consumer = KafkaConsumer(
     'matchmaking_requests',
-    bootstrap_servers = 'localhost:9092',
+    bootstrap_servers = 'kafka',
     value_deserializer = json_deserializer,
-    key_deserializer = json_deserializer
+    key_deserializer = json_deserializer,
+    group_id = "matchmaker",
 )
 
 producer = KafkaProducer(
-    bootstrap_servers = 'localhost:9092',
+    bootstrap_servers = 'kafka',
     value_serializer = json_serializer,
-    key_serializer = json_serializer
+    key_serializer = json_serializer,
 )
 
 for msg1, msg2 in pairwise(consumer):
     paired = [msg1.value, msg2.value]
+    eprint("sending pair", msg1.value, msg2.value)
     producer.send('matchmaking_pairs', paired)
+    producer.flush()
+    consumer.commit()
 
