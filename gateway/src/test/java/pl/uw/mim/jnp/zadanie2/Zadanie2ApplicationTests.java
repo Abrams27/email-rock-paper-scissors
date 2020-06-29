29a7aab530c9efd3f48574e0845b0a800ec77dd9
Abrams27 <abramowiczmarcin99@gmail.com>
2020-06-29 17:31:35 +0200

    init

---
+++ gateway/src/test/java/pl/uw/mim/jnp/zadanie2/Zadanie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uw.mim.jnp.zadanie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
